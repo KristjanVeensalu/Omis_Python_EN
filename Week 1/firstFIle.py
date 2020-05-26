707d5340bc59d642bd0fc9085b7d8f10c1e5fd68
mihkelva <51737622+mihkelva@users.noreply.github.com>
2020-05-26 20:43:17 +0300

    First lesson

---
+++ Week 1/firstFIle.py
@@ -0,0 +1,60 @@
+#This is a comment
+
+'''print("Hello world!")
+print(5)
+print(5+5)
+print("Testing if this works" + "5")
+print(10*2-15)'''
+
+'''MyFirstVariable = 10
+print(MyFirstVariable)
+MySecondVariable = "This is my variable"
+
+print(MySecondVariable)
+
+MySecondVariable = 20
+
+print(MySecondVariable)'''
+'''
+firstSide = 10
+print(firstSide*firstSide)
+
+triangleSide= 20
+height = 15
+print("The plane of this triangle is:")
+print(triangleSide*height/2)
+
+
+if height<20: 
+    print("That height is small")
+if height>500:
+    print("That is very high")
+
+UserInput=int(input("What is your input? "))
+
+if UserInput<20:
+    print("That is a small input")
+
+if UserInput>20:
+    print("That is a big input")'''
+
+FirstBooleanVariable = True
+
+if FirstBooleanVariable:
+    print("Our boolean works!")
+
+
+'''Write a piece of code, that asks for user input. If that Input,divided by 4
+and multiplied by 10, is still smaller than 1000. Then print out the value of the
+remaining variable number'''
+
+'''Find out if this number is divisible by 4 but not by 100'''
+
+UserSecondInput = int(input("Enter your number "))
+
+if UserSecondInput % 4 == 0:
+    print("Is divisible by 4!")
+    if UserSecondInput % 100 != 0:
+        print("This is the value!")
+
+
