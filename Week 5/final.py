ab4a10a278674203c6ee3f6cb524cd3484d565b3
Kristjan Veensalu <Kristjan.Veensalu@prike.ee>
2020-06-26 10:45:40 +0300

    Deleted old files

--- Week 5/final.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-URL = 'graph.facebook.com'
-
-fetchedData = requests.get(URL)
-
-soup = BeautifulSoup(fetchedData.content, 'html.parser')
-
-print(soup.prettify())
