27de6206a718e482aadbcaf95f8eacad37efaf4a
mihkelva <51737622+mihkelva@users.noreply.github.com>
2020-06-30 17:36:48 +0300

    Update

---
+++ Week 5/components.py
@@ -0,0 +1,2 @@
+LOGIN = 'kristjan.veensalu@gmail.com'
+PASS = ''
--- Week 5/redux.py
+++ Week 5/redux.py
@@ -5,36 +5,39 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 from bs4 import BeautifulSoup as bs
-
+import components
 
 chrome_options = webdriver.ChromeOptions()
 prefs = {"profile.default_content_setting_values.notifications": 2}
 chrome_options.add_experimental_option("prefs", prefs)
-
 driver = webdriver.Chrome(options=chrome_options)
+driver.implicitly_wait(3)
 LINK = 'https://www.facebook.com/login.php?login_attempt=1&lwv=111'
-LOGIN = "kristjan.veensalu@gmail.com"
-PASS = "ENTER PASSWORD HERE"
+LOGIN = components.LOGIN
+PASS = components.PASS
 
 
 
 def login(LOGIN_URL, login, password):
     driver.get(LOGIN_URL)
     # wait for the login page to load
-    sleep(3)
     driver.find_element_by_id('email').send_keys(login)
     driver.find_element_by_id('pass').send_keys(password)
     driver.find_element_by_id('loginbutton').click()
 
 login(LINK, LOGIN, PASS)
-sleep(3)
 driver.find_element_by_class_name('_1vp5').click()
-sleep(3)
 driver.find_elements_by_class_name('_6-6')[2].click()
-sleep(3)
 for x in range(30):
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     html = driver.execute_script("return document.documentElement.innerHTML;")
+driver.close()
 soup = bs(html, "html.parser")
 
-print(soup.prettify()) #The variable with the text in it is soup.
+#print(soup.prettify()) #The variable with the text in it is soup.
+
+listOfLinks = []
+
+for div in soup.find_all('div',{"class": "fsl fwb fcb"}):
+    print(div)
+print (listOfLinks)
